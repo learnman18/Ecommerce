8804b100dbe769189b4c97a341b44cd851d24dd5
learnman18 <manishpan18@gmail.com>
2024-03-25 20:10:15 +0530

    [25-03]-Navbar of the page

---
+++ src/components/HomePage/LandingPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import firstImg from "../Images/features-light-1.jpg"
+import coverImg from "../Images/features-light-2.jpg"
+import "../style.css"
+
+const LandingPage = () => {
+
+    return(
+        <>
+            <div className="row gy-4 align-items-center features-item">
+                <div className="col-lg-5 order-2 order-lg-1 aos-init" data-aos="fade-up" data-aos-delay="200">
+                    <h3>Corporis temporibus maiores provident</h3>
+                    <p>
+                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
+                    </p>
+                    <a href="/" className="btn btn-get-started">Get Started</a>
+                </div>
+                <div className="col-lg-7 order-1 order-lg-2 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
+                    <div className="image-stack">
+                    <img src={firstImg} alt="" className="stack-front" />
+                    <img src={coverImg} alt="" className="stack-back" />
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default LandingPage;
