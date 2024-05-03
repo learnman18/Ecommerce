4fc2e3219985c42fede4fc9ef9b363f68f72f990
learnman18 <manishpan18@gmail.com>
2024-05-03 16:47:56 +0530

    [03-05]-routeUpdateToTest

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -5,8 +5,8 @@ const CartToast = (props) => {
     return (
         <>
             <div className={`alert alert-warning alert-dismissible fade ${props.displayALertMsg }`} role="alert">
-                <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                {props.selectedProductName} Product added to cart
+                {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
             </div>
         </>
     );
