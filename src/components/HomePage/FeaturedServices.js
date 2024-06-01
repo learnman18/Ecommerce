51a0ebca1389511ac7a07c68f06342ad511392af
learnman18 <manishpan18@gmail.com>
2024-06-01 14:38:18 +0530

    [01-06]-totalAmountOfProducts

--- src/components/HomePage/FeaturedServices.js
+++ src/components/HomePage/FeaturedServices.js
@@ -125,7 +125,7 @@ const FeaturedItems = () => {
             <div className="mt-5 featured">
                 <div className="container section-title">
                     <h2>Portfolio</h2>
-                    <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+                    <p>Some home page products that can be bought</p>
                 </div> 
                 <div className="compTwoContainer mt-5">
                     <div className="row gy-4 isotope-container" style={parentPosition}>
