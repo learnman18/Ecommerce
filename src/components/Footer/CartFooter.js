88e4c2f55b6e5310c34ed2f73cb680ebfb5db00d
learnman18 <manishpan18@gmail.com>
2024-08-06 20:03:03 +0530

    classToClassName

--- src/components/Footer/CartFooter.js
+++ src/components/Footer/CartFooter.js
@@ -5,20 +5,20 @@ import { Link } from "react-router-dom";
 export default function CartFooter(){
     return(
         <React.Fragment>
-            <footer class="mainCartFooter">
-                <div class="cartFooter">
+            <footer className="mainCartFooter">
+                <div className="cartFooter">
                     <div>
                         <div>
-                            <div class="footerItems">
-                                <div class="col col-8-12">
+                            <div className="footerItems">
+                                <div className="col col-8-12">
                                     <span>
-                                        <span class="policies-title footertitle">Policies:</span>
-                                        <Link class="Pyr2uI" to="" target="_blank" rel="noopener noreferrer">Returns Policy</Link>
-                                        <Link class="Pyr2uI" to="" target="_blank" rel="noopener noreferrer">Terms of use</Link>
-                                        <Link class="Pyr2uI" to="" target="_blank" rel="noopener noreferrer">Security</Link>
-                                        <Link class="Pyr2uI" to="" target="_blank" rel="noopener noreferrer">Privacy</Link>
-                                        <Link class="Pyr2uI" to="" target="_blank" rel="noopener noreferrer">Infringement</Link>
-                                        </span><span class="_1mBSCr">© 2024 <span>Ecommerce.com</span>
+                                        <span className="policies-title footertitle">Policies:</span>
+                                        <Link className="Pyr2uI" to="" target="_blank" rel="noopener noreferrer">Returns Policy</Link>
+                                        <Link className="Pyr2uI" to="" target="_blank" rel="noopener noreferrer">Terms of use</Link>
+                                        <Link className="Pyr2uI" to="" target="_blank" rel="noopener noreferrer">Security</Link>
+                                        <Link className="Pyr2uI" to="" target="_blank" rel="noopener noreferrer">Privacy</Link>
+                                        <Link className="Pyr2uI" to="" target="_blank" rel="noopener noreferrer">Infringement</Link>
+                                        </span><span className="_1mBSCr">© 2024 <span>Ecommerce.com</span>
                                     </span>
                                 </div>
                             </div>
